ba77fc71ce54bcb2db20aaec8af107e64b3e03f1
mesut <yildirimesutcw@gmail.com>
2022-04-06 23:18:12 +0300

    about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import aboutLogo from "../../assets/coding.svg"
+import styling from "./About.module.scss"
 
 const About = () => {
   return (
-    <div>
-     <h1>About Software Developer Mesut</h1>
+    <div className={styling.big_box}>
+    <img className={styling.about_logo} src={aboutLogo} alt="logo" />
+     <h1 className={styling.text}>About Software Developer <span className={styling.name}> Mesut</span> </h1>
+     <div className={styling.text_box}>
+      <h2>Hi, I'am Mesut</h2>
+      <h3>I’m currently learning Full-Stack Development Languages.</h3>
+      <h3>I know JS, ReactJS, Django, NodeJS, MongoDB,SQL, Python, AWS Services.</h3>
+      <h3><a href="mailto:yildirimesutcw@gmail.com">Send email</a> : yildirimesutcw@gmail.com</h3>
+
+     </div>
+    
     
         
    </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styling from "./Home.module.scss"
 
 const Home = () => {
   return (
